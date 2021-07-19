a0eaa3d15b8ab53b5fa3fb1b2348f74a3c02ba25
Chetan Patel <Chetan@Chetans-MacBook-Air.local>
2021-07-19 06:38:39 -0400

    api routes should likely be finished

--- routes/api.js
+++ routes/api.js
@@ -1,8 +1,48 @@
 const router = require("express").Router();
-const Transaction = require("../models/schema.js");
+const Transaction = require("../models/budgetModel.js");
 
 router.post("/api/transaction", ({ body }, res) => {
   //do stuff
+  console.log("entered the router.post route for transaction");
+  Transaction.create(body)
+    .then(dbTransaction => {
+      res.json(dbTransaction);
+    })
+    .catch(err => {
+      res.status(404).json(err);
+    });
+
+});
+
+
+/**
+ * This method is for bulk post req to the api
+ */
+router.post("/api/transaction/bulk", ({body}, res) => {
+  console.log("entered the router.post route for bulk transactions");
+  Transaction.insertMany(body)
+    .then(dbTransaction => {
+      res.json(dbTransaction);
+    })
+    .catch(err => {
+      console.log("error api.js line 25");
+      res.status(404).json(err);
+    });
+});
+
+/**
+ * this api's main get method. returns all of them sorted in decending order.
+ */
+router.get("/api/transaction", (req, res) => {
+  
+  Transaction.find({}).sort({date: -1})
+    .then(dbTransaction => {
+      res.json(dbTransaction);
+    })
+    .catch(err => {
+      res.status(404).json(err);
+    });
+
 });
 
 
